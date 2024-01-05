bf6dd69a28c836efa34e5de7ecbfbb0fff81e8f1
tanhLinhphong <pvtuananh1505@gmail.com>
2024-01-05 10:05:03 +0700

    Update shoppingcartv2.php

--- demo1/shoppingcartv2.php
+++ demo1/shoppingcartv2.php
@@ -1,51 +1,48 @@
 <?php
-// Mảng chứa thông tin sản phẩm
+session_start();
+
+
 $products = array(
     array("id" => 1, "name" => "Áo Polo", "price" => 25),
-    array("id" => 2, "name" => "Quần Jeans", "price" => 50),
-    array("id" => 3, "name" => "Giày Sneakers", "price"=> 40)
+    array("id" => 2, "name" => "Quần jeans", "price" => 50),
+    array("id" => 3, "name" => "Giày Sneakers", "price" => 40)
 );
 
-// Kiểm tra giỏ hàng chưa được tạo, thì tạo mới
-if (!isset($_SESSION['cart'])) {
+if (!isset($_SESSION['cart']))  {
     $_SESSION['cart'] = array();
 }
 
-// Hàm thêm sản phẩm vào giỏ hàng
 function addToCart($productId)
 {
     global $products;
-
-    foreach ($products as $product) {
+    foreach ($products as  $product) {
         if ($product['id'] == $productId) {
             $_SESSION['cart'][] = $product;
-            echo "đã thêm ". $product['name'] ." vào giỏ hàng.";
+            echo "Đã thêm" . $product['name'] . "vào giỏ hàng.";
             return;
         }
     }
+    echo "Sản phẩm không tồn tại";
 
-    echo "sản phẩm không tồn tại!";
 }
 
-// Hàm hiển thị giỏ hàng
 function viewCart()
 {
     if (empty($_SESSION['cart'])) {
-        echo 'Giỏ hàng trống.';
-        return;
-    } else {
-        echo "<h2>Danh sách các bạn:</h2>";
+        echo "Giỏ hàng trống.";
+    }else {
+        echo "<h2>Giỏ hàng của bạn:</h2>";
         $totalPrice = 0;
 
-        foreach ($_SESSION['cart'] as $item) {
-            echo $item['name'] . "-$" . $item['price'] . "<br>";
+        foreach  ($_SESSION['cart'] as $item) {
+            echo $item['name'] . "- $" . $item['price'] . "<br>";
             $totalPrice += $item['price'];
         }
-
-        echo "<strong>Tổng tiền:$ </strong>" . $totalPrice;
+        echo "<strong>Tổng giá trị: $" . $totalPrice . "</strong>";
     }
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -74,7 +71,6 @@ viewCart();
 ?>
 </body>
 </html>
-
 
 
 
